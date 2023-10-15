b3994ca2921825b40ac2efb9479fa47d9158ac64
Sleeme2020 <sleeme2020@yandex.ru>
2023-10-15 11:11:20 +0300

    Create

--- app/src/main/java/com/example/myapplication3/infrasruct.kt
+++ app/src/main/java/com/example/myapplication3/infrasruct.kt
@@ -1,7 +1,6 @@
 package com.example.myapplication3
 
-class infrasruct {
-}
+
 
 interface OnLinkFragment {
     fun onLinkFragment(link: String?)
---
+++ app/src/main/java/com/example/myapplication3/work/mainwork.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication3.work
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication3.R
+
+class mainwork: AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mainwork)
+
+    }
+}
